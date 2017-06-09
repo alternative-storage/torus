2a27c1b4990441f3365b2e6ac23cea4e879f6653
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2017-06-09 21:40:53 +0900

    grpc: Update grpc egenerated code

--- models/generate.sh
+++ models/generate.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+PATH=${PATH}:${GOBIN}
 GOGOPROTO_ROOT="${GOPATH}/src"
 GOGOPROTO_PATH="${GOGOPROTO_ROOT}:${GOGOPROTO_ROOT}/protobuf"
-protoc --gogofaster_out=plugins=grpc:. -I=.:"${GOGOPROTO_PATH}" *.proto
+
+protoc --gogofaster_out=plugins=grpc:. -I. -I${GOPATH}/src -I${GOPATH}/src/github.com/gogo/protobuf/protobuf *.proto
+#protoc --gogofaster_out=plugins=grpc:. -I=.:"${GOGOPROTO_PATH}" *.proto
