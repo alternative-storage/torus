766c0ad46e6cbbf32b95997defb9d76b461b9984
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2017-06-18 14:25:24 +0900

    ring: update ketama test

--- ring/ketama_test.go
+++ ring/ketama_test.go
@@ -1,6 +1,8 @@
 package ring
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/alternative-storage/torus"
@@ -8,7 +10,7 @@ import (
 	"github.com/serialx/hashring"
 )
 
-func TestTinyPeer(t *testing.T) {
+func makeTinyPeers(t *testing.T) torus.Ring {
 	pi := torus.PeerInfoList{
 		&models.PeerInfo{
 			UUID:        "a",
@@ -23,12 +25,16 @@ func TestTinyPeer(t *testing.T) {
 			TotalBlocks: 100 * 1024 * 2,
 		},
 	}
-	k := &ketama{
+	return &ketama{
 		version: 1,
 		peers:   pi,
 		rep:     2,
 		ring:    hashring.NewWithWeights(pi.GetWeights()),
 	}
+}
+
+func TestGetPeer(t *testing.T) {
+	k := makeTinyPeers(t)
 	l, err := k.GetPeers(torus.BlockRef{
 		INodeRef: torus.NewINodeRef(3, 4),
 		Index:    5,
@@ -37,4 +43,52 @@ func TestTinyPeer(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Log(l.Peers)
+}
+
+func TestAddPeers(t *testing.T) {
+	newpi := torus.PeerInfoList{
+		&models.PeerInfo{
+			UUID:        "d",
+			TotalBlocks: 20 * 1024 * 1024 * 2,
+		},
+	}
+	k := makeTinyPeers(t).(torus.RingAdder)
+	newk, err := k.AddPeers(newpi)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if newk.Version() != 2 {
+		t.Fatalf("Faild to update version to %d, expected %d", newk.Version(), 2)
+	}
+}
+
+func TestRemovePeers(t *testing.T) {
+	k := makeTinyPeers(t).(torus.RingRemover)
+	rmpi := torus.PeerList{"a"}
+	newk, err := k.RemovePeers(rmpi)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if newk.Version() != 2 {
+		t.Fatalf("Faild to update version to %d, expected %d", newk.Version(), 2)
+	}
+	pl := newk.Members()
+	if len(pl) != 2 {
+		t.Fatalf("Faild to remove peer member")
+	}
+}
+
+func TestChangeReplication(t *testing.T) {
+	newRep := 3
+	k := makeTinyPeers(t).(torus.ModifyableRing)
+	newk, err := k.ChangeReplication(newRep)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if newk.Version() != 2 {
+		t.Fatalf("Faild to update version to %d, expected %d", newk.Version(), 2)
+	}
+	if !strings.Contains(newk.Describe(), fmt.Sprintf("Ring: Ketama\nReplication:%d\nPeers:", newRep)) {
+		t.Fatalf("Got wrong replications")
+	}
 }
